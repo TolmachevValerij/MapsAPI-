fb2f1667c90de13185948feef34db8afdb9e81be
TolmachevValerij <98163361+TolmachevValerij@users.noreply.github.com>
2023-02-11 18:00:35 +0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,40 +1,49 @@
-import pygame
-import requests
-import sys 
 import os
+import sys
+import requests
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5 import uic
 
 
+class APIMap(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.x, self.y, self.m = '65.537696', '57.151173', '0.002'
+        uic.loadUi('maps.ui', self)
+        self.pushButton.clicked.connect(self.setImageToPixmap)
+        self.map_request = ['http://static-maps.yandex.ru/1.x/?ll=', self.x, ',', self.y,
+                            '&spn=', self.m, ',', self.m, '&l=map']
 
-def load_map():
-    map_request = "https://static-maps.yandex.ru/1.x/?l=map&pt=65.534121,57.149484~65.540921,57.153502~65.537696,57.151173"
-    response = requests.get(map_request)
-    if not response:
-        print("Ошибка выполнения запроса:")
-        print(map_request)
-        print("Http статус:", response.status_code, "(", response.reason, ")")
-        sys.exit(1)
-
-    map_file = "map.png"
-    try:
-        with open(map_file, "wb") as file:
+    def getImage(self):
+        self.x = self.edit_x.toPlainText()
+        self.y = self.edit_y.toPlainText()
+        self.m = self.mashtab.toPlainText()
+        self.map_request = ''.join(['http://static-maps.yandex.ru/1.x/?ll=', self.x, ',',
+                                    self.y, '&spn=', self.m, ',', self.m, '&l=map'])
+        response = requests.get(self.map_request)
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(self.map_request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            return "Ошибка выполнения запроса:" + '\n' + "Http статус:", response.status_code, "(", response.reason, ")"
+        self.map_file = "map.png"
+        with open(self.map_file, "wb") as file:
             file.write(response.content)
-    except IOError as ex:
-        print("Ошибка записи временного файла:", ex)
-        sys.exit(2)
-    return map_file
+        return 'успех'
+    def setImageToPixmap(self):
+        is_all_secc = self.getImage()
+        if is_all_secc == 'успех':
+            self.pixmap = QPixmap(self.map_file)
+            self.image.setPixmap(self.pixmap)
+        else:
+            self.image.setText(is_all_secc)
+    def closeEvent(self, event):
+        os.remove(self.map_file)
 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((600, 450))
-    while True:
-        event = pygame.event.wait()
-        if event.type == pygame.QUIT:
-           break
-        map_file = load_map()
-        screen.blit(pygame.image.load(map_file), (0, 0))
-        pygame.display.flip()
-    pygame.quit(
-    os.remove(map_file)
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = APIMap()
+    ex.show()
+    sys.exit(app.exec())
