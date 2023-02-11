662473c3aae12ba9becc32946641c6cbd724dbbe
TolmachevValerij <98163361+TolmachevValerij@users.noreply.github.com>
2023-02-11 18:44:11 +0500

    2

--- 1.py
+++ 1.py
@@ -9,11 +9,26 @@ from PyQt5 import uic
 class APIMap(QWidget):
     def __init__(self):
         super().__init__()
-        self.x, self.y, self.m = '65.537696', '57.151173', '0.002'
+        self.x, self.y, self.m = '37.530887', '55.703118', '0.002'
         uic.loadUi('maps.ui', self)
         self.pushButton.clicked.connect(self.setImageToPixmap)
         self.map_request = ['http://static-maps.yandex.ru/1.x/?ll=', self.x, ',', self.y,
                             '&spn=', self.m, ',', self.m, '&l=map']
+        self.setImageToPixmap()
+        
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp:
+            try:
+                self.m.setPlainText(str(float(self.mashtab.toPlainText()) + 0.01))
+                self.setImageToPixmap()
+            except FloatingPointError as e:
+                print(e)
+        elif event.key() == Qt.Key_PageDown:
+            try:
+                self.mashtab.setPlainText(str(float(self.m.toPlainText()) - 0.01))
+                self.setImageToPixmap()
+            except FloatingPointError as e:
+                print(e)
 
     def getImage(self):
         self.x = self.edit_x.toPlainText()
@@ -31,15 +46,28 @@ class APIMap(QWidget):
         with open(self.map_file, "wb") as file:
             file.write(response.content)
         return 'успех'
+    
     def setImageToPixmap(self):
         is_all_secc = self.getImage()
         if is_all_secc == 'успех':
             self.pixmap = QPixmap(self.map_file)
             self.image.setPixmap(self.pixmap)
         else:
-            self.image.setText(is_all_secc)
+            win = WarningWindow(self, is_all_secc)
+            win.show()
+         
     def closeEvent(self, event):
         os.remove(self.map_file)
+
+        
+class WarningWindow(QMainWindow):
+    def __init__(self, main, text):
+        super().__init__(main)
+        self.setGeometry(50, 50, 500, 500)
+        self.warning = QLabel(self)
+        self.warning.setText(text)
+        self.warning.resize(self.warning.sizeHint())
+        self.warning.move(250 - self.warning.width() // 2, 250 - self.warning.height() // 2)
 
 
 if __name__ == '__main__':
